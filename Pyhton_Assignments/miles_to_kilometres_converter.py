29aa89c406e94f1b7ac8b2891e231aaee56dab2d
Damian <ndebuezedamian@gmail.com>
2022-12-05 11:57:20 +0100

    initial

--- Pyhton_Assignments/miles_to_kilometres_converter.py
+++ Pyhton_Assignments/miles_to_kilometres_converter.py
@@ -2,4 +2,4 @@ for i in range(1, 11, 1):
     miles_kilometres = i * 1.609
     print(f" {i}  \t {miles_kilometres: .3f}")
 # \t keyword for tab or space
-# f" the printf format for python
+# "f" the printf format for python
